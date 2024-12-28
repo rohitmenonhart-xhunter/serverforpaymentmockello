40d9779b2762204b191dbf5affbe8e299c4b0ecf
Rohit menon <140264361+rohitmenonhart-xhunter@users.noreply.github.com>
2024-12-29 01:39:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import uuid
 import time
 import concurrent.futures
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(app)
@@ -101,4 +102,5 @@ def ping():
 
 
 if __name__ == '__main__':
-    app.run(debug=True, threaded=True)
+    port = int(os.environ.get('PORT', 5000))  # Use PORT environment variable if set, otherwise default to 5000
+    app.run(host='0.0.0.0', port=port, debug=True, threaded=True)
