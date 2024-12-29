142154e592b55beeec95221b2a7990945ceb2a7b
Rohit menon <140264361+rohitmenonhart-xhunter@users.noreply.github.com>
2024-12-29 12:57:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -149,5 +149,5 @@ def ping():
     return jsonify({"status": "Server is up and running!"}), 200
 
 if __name__ == '__main__':
-port = int(os.environ.get('PORT', 5000)) # Use PORT environment variable if set, otherwise default to 5000
-app.run(host='0.0.0.0', port=port, debug=True, threaded=True)
+    port = int(os.environ.get('PORT', 5000))  # Use PORT environment variable if set, otherwise default to 5000
+    app.run(host='0.0.0.0', port=port, debug=True, threaded=True)
